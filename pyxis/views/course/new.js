29a211c32b40f0d900c6f2c18593026f9f6f6632
Fábio Luiz Fischer <feuture@hotmail.com>
2017-06-12 21:19:09 -0300

    new course screen

--- pyxis/views/course/new.js
+++ pyxis/views/course/new.js
@@ -1,28 +1,78 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Alert, Text, View, Button, StyleSheet } from 'react-native';
+import Components from './../../components';
+import TextField from './../../components/TextField'; 
+
+import CourseServices from './../../services/course';
 
 const styles = StyleSheet.create({
   base: {
     padding: 24
   },
-  name: {
+  header: {
+    marginBottom: 16
+  },
+  title: {
     fontSize: 24
   }
 });
 
-class NewCourseScreen extends React.Component {
+class NewCourseScreen extends Components.PyxisComponent {
   static navigationOptions = {
     title: 'Novo curso'
   };
 
   constructor(props) {
     super(props);
+
+    const { state } = this.props.navigation;
+
+    this.state = {
+      instituteId: state.params ? state.params.instituteId : -1,
+      name: ''
+    };
+  }
+
+  onFieldChange(event) {
+    const changeObject = {};
+    changeObject[event.fieldName] = event.fieldValue;
+
+    this.setState(changeObject);
+  }
+
+  async createinstitute() {
+    try {
+      const coordinator = await this.service.coordinatorsRepository.save({
+        user_id: this.services.currentuser.id
+      });
+
+      const course = await this.services.coursesRepository.save({
+        name: this.state.name,
+        institute: this.state.instituteId,
+        coordinator_id: coordinator.id
+      });
+
+      Alert.alert('Sucesso!');
+    } catch (err) {
+      Alert.alert('Oops', err.message);
+    }
   }
 
   render() {
     return (
-      <View style={styles.base}>
+      <View style={style.base}>
+        <View style={style.header}>
+          <Text style={style.title}>Novo curso</Text>
+        </View>
         <View>
+          <TextField 
+            name="name"
+            placeholder="Nome"
+            value={this.state.name}
+            onChange={e => this.onFieldChange(e)}>
+          </TextField>
+          <Button title="Salvar" onPress={() => this.createCourse()}></Button>
+          <Button title="Voltar" onPress={() => this.navigate('Home')}></Button>
         </View>
       </View>
     );
